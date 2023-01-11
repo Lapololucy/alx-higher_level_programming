841568bf7b5393f58b1a942d4816d2679d19c9c2
Lapololucy <lapololuucy@gmail.com>
2023-01-10 22:47:58 -0800

    python more data structure

--- 0x04-python-more_data_structures/0-square_matrix_simple.py
+++ 0x04-python-more_data_structures/0-square_matrix_simple.py
@@ -1,4 +1,4 @@
 #!/usr/bin/python3
 def square_matrix_simple(matrix=[]):
-        if matrix:
-                    return [list(map(lambda j: j**2, i)) for i in matrix]
+    if matrix:
+        return [list(map(lambda j: j**2, i)) for i in matrix]
