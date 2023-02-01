1f6e2047ea324dff9835dccb6ba538f54c137627
Lapololucy <lapololucy@gmail.com>
2023-01-31 22:08:59 -0800

    tried

--- 0x07-python-test_driven_development/100-matrix_mul.py
+++ 0x07-python-test_driven_development/100-matrix_mul.py
@@ -1,68 +1,66 @@
 #!/usr/bin/python3
 """
-    100-matrix_mul Module
+This module multiplies 2 matrix
+Raises ValueError TypeError
 """
 
 
 def matrix_mul(m_a, m_b):
     """
-        Multiplies 2 matrices
-        Args:
-            m_a: first matrix(2D List)
-            m_b: second matrix(2D List)
-        Returns:
-            the product of two matrices
+    function to multiply 2 matrix
     """
-
-    prev_len = 0
+    listErr = "{} must be a list"
+    emptyErr = "{} can't be empty"
+    typeErr = "{} should contain only integers or floats"
+    sizeErr = "each row of {} must should be of the same size"
+    
     if type(m_a) is not list:
-        raise TypeError("m_a must be a list")
+        raise TypeError(listErr.format('m_a'))
     if type(m_b) is not list:
-        raise TypeError("m_b must be a list")
-    if not m_a:
-        raise ValueError("m_a can't be empty")
-    if not m_b:
-        raise ValueError("m_b can't be empty")
+        raise TypeError(listErr.format('m_b'))
 
-    for blocks in m_a:
-        if type(blocks) is not list:
-            raise TypeError("m_a must be a list of lists")
-        if not blocks:
-            raise ValueError("m_a can't be empty")
-        for integers in blocks:
-            if type(integers) is not int and type(integers) is not float:
-                raise TypeError("m_a should contain only integers or floats")
-        if len(blocks) != prev_len and prev_len != 0:
-            raise TypeError("each row of m_a must be of the same size")
-        prev_len = len(blocks)
+    if len(m_a) == 0 or type(m_a[0]) is list and len(m_a[0]) == 0:
+        raise ValueError(emptyErr.format('m_a'))
+    if len(m_b) == 0 or type(m_b[0]) is list and len(m_b[0]) == 0:
+        raise ValueError(emptyErr.format('m_b'))
 
-    prev_len = 0
-    for blocks in m_b:
-        if type(blocks) is not list:
-            raise TypeError("m_b must be a list of lists")
-        if not blocks:
-            raise ValueError("m_b can't be empty")
-        for integers in blocks:
-            if type(integers) is not int and type(integers) is not float:
-                raise TypeError("m_b should contain only integers or floats")
-        if len(blocks) != prev_len and prev_len != 0:
-            raise TypeError("each row of m_b must be of the same size")
-        prev_len = len(blocks)
+    s = -1
 
-    if len(m_a[0]) != len(m_b):  # cols of m_a must be equal to rows of m_b
-        raise ValueError("m_a and m_b can't be multiplied")
+    for x in m_a:
+        if type(x) is list:
+            if s == -1:
+                s = len(x)
+            else:
+                if s != len(x):
+                    raise TypeError(sizeErr.format('m_a'))
+            for y in x:
+                if not isinstance(y, (int, float)):
+                    raise TypeError(typeErr.format('m_a'))
+        else:
+            raise TypeError(typeErr.format('m_a'))
 
-    result_list = list()    # multiplication starts
-    for row_a in range(len(m_a)):
-        flag = 0
-        inner_list = list()
-        for row_b in range(len(m_b)):
-            for col in range(len(m_b[row_b])):
-                if flag == 0:
-                    inner_list.append(m_a[row_a][row_b] * m_b[row_b][col])
-                else:
-                    inner_list[col] += (m_a[row_a][row_b] * m_b[row_b][col])
-            flag = 1
-        result_list.append(inner_list)  # multiplied matrix(2D List)
+    s = -1
 
-    return result_list
+    for x in m_b:
+        if isinstance(x, list):
+            if s == -1:
+                s = len(x)
+            else:
+                if s != len(x):
+                    raise TypeError(sizeErr.format('m_b'))
+            for y in x:
+                if not isinstance(y, (int, float)):
+                    raise TypeError(typeErr.format('m_b'))
+        else:
+            raise TypeError(typeErr.format('m_b'))
+
+    if len(m_a[0]) != len(m_b):
+        raise ValueError('m_a and m_b can\'t be multiplied')
+
+    final = [[0 for a in m_b[0]] for x in m_a]
+    for i in range(len(m_a)):
+        for n in range(len(m_b[0])):
+            for k in range(len(m_b)):
+                final[i][n] += m_a[i][k] * m_b[k][n]
+
+    return final
