67127c6bc9fa3b8bfa4e22e94e9927cfccc771c8
Lapololucy <lapololucy@gmail.com>
2023-04-10 23:46:09 -0700

    Task 1, Multi-languages

---
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+const line1 = "C is fun";
+const line2 = "Python is cool";
+const line3 = "JavaScript is amazing";
+
+console.log(line1);
+console.log(line2);
+console.log(line3);
+
