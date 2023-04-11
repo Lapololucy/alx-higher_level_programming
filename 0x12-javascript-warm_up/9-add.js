7523133ed85268cb1c14a541dc570e038a833fa8
Lapololucy <lapololucy@gmail.com>
2023-04-11 08:45:01 -0700

    Task 9

--- 0x12-javascript-warm_up/9-add.js
+++ 0x12-javascript-warm_up/9-add.js
@@ -1,16 +1,7 @@
 #!/usr/bin/node
-function add(a, b) {
-  return a + b;
+function add (a, b) {
+	const c = a + b;
+	console.log(c);
 }
 
-const arg1 = process.argv[2];
-const arg2 = process.argv[3];
-
-if (Number.isInteger(parseInt(arg1)) && Number.isInteger(parseInt(arg2))) {
-  const num1 = parseInt(arg1);
-  const num2 = parseInt(arg2);
-  console.log(add(num1, num2));
-} else {
-  console.log("Invalid input, please provide two integers");
-}
-
+add(Number(process.argv[2]), Number(process.argv[3]));
