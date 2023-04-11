4ec2b219cba297f048ee0ee0e86da11f662f8f75
Lapololucy <lapololucy@gmail.com>
2023-04-11 09:48:10 -0700

    test

--- 0x12-javascript-warm_up/101-call_me_moby.js
+++ 0x12-javascript-warm_up/101-call_me_moby.js
@@ -1,6 +1,9 @@
 #!/usr/bin/node
-exports.callMeMoby = function (x, theFunction) {
-	for (let i = 0; i< x; i++) {
-		theFunction();
-	}
-};
+
+function repeat(x, theFunction) {
+  for (let i = 0; i < x; i++) {
+    theFunction();
+  }
+}
+
+module.exports = repeat;
