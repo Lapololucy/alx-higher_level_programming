f43accac0759b7dc9fee2037ff48bc29c4056788
Lapololucy <lapololucy@gmail.com>
2023-04-11 08:18:36 -0700

    Task 5

--- 0x12-javascript-warm_up/5-to_integer.js
+++ 0x12-javascript-warm_up/5-to_integer.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
-if (isNaN(process.argv[2]) || process.argv[2] === underfined) {
-	console.log('Not a number');
+if (isNaN(process.argv[2]) || process.argv[2] === undefined) {
+  console.log('Not a number');
 } else {
-	console.log('My number:' parseInt(process.argv[2]));
+  console.log('My number:', parseInt(process.argv[2]));
 }
