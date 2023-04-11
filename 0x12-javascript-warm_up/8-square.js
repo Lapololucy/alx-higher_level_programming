c4bbad8b9291eb0e7862f9a7cef5968d41cfcd02
Lapololucy <lapololucy@gmail.com>
2023-04-11 08:37:25 -0700

    fixing errors

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -4,7 +4,7 @@ const arg = process.argv[2];
 if (Number.isInteger(parseInt(arg))) {
   const size = parseInt(arg);
   for (let i = 0; i < size; i++) {
-    let row = '';
+    let row = "";
     for (let j = 0; j < size; j++) {
       row += 'X';
     }
