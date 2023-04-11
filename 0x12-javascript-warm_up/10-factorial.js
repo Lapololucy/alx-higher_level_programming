efe11eab35cbf62afe07ba5627554578df3709d3
Lapololucy <lapololucy@gmail.com>
2023-04-11 00:31:27 -0700

    Task 10, a script that computes and prints a factorial

---
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+function factorial(n) {
+  if (isNaN(n)) {
+    return 1;
+  } else if (n === 0) {
+    return 1;
+  } else {
+    return n * factorial(n - 1);
+  }
+}
+
+const arg = process.argv[2];
+const num = parseInt(arg);
+
+console.log(factorial(num));
+
