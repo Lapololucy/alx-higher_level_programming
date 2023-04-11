9c1018cc24ec41be2bd5a563c10864e314c74c92
Lapololucy <lapololucy@gmail.com>
2023-04-11 08:39:42 -0700

    error fixed

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,16 +1,11 @@
 #!/usr/bin/node
-const arg = process.argv[2];
-
-if (Number.isInteger(parseInt(arg))) {
-  const size = parseInt(arg);
-  for (let i = 0; i < size; i++) {
-    let row = "";
-    for (let j = 0; j < size; j++) {
-      row += 'X';
-    }
-    console.log(row);
-  }
-} else {
+if (process.argv[2] === undefined || isNaN(process.argv[2])) {
   console.log('Missing size');
+} else {
+  const x = Number(process.argv[2]);
+  let i = 0;
+  while (i < x) {
+    console.log('X'.repeat(x));
+    i++;
+  }
 }
-
