3d7d3d5f3567b8a4f376c229e7bb0d43d1d0784a
Lapololucy <lapololucy@gmail.com>
2023-04-11 05:42:13 -0700

    Task fixed

--- 0x12-javascript-warm_up/1-multi_languages.js
+++ 0x12-javascript-warm_up/1-multi_languages.js
@@ -1,5 +1,4 @@
 #!/usr/bin/node
-console.log('C is fun');
-console.log('Python is cool');
-console.log('JavaScript is amazing');
 
+const languages = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+for (const lang of languages) console.log(lang);
