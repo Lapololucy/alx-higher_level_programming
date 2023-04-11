a03aa1dbefbc3aab2030f6a140784923daa64749
Lapololucy <lapololucy@gmail.com>
2023-04-11 00:22:23 -0700

    Task 8, script that prints a square

---
+++ 0x12-javascript-warm_up/8-square.js
@@ -0,0 +1,16 @@
+#!/usr/bin/node
+const arg = process.argv[2];
+
+if (Number.isInteger(parseInt(arg))) {
+  const size = parseInt(arg);
+  for (let i = 0; i < size; i++) {
+    let row = "";
+    for (let j = 0; j < size; j++) {
+      row += "X";
+    }
+    console.log(row);
+  }
+} else {
+  console.log("Missing size");
+}
+
