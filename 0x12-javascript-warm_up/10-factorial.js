1b0cb9d550eb5469def8dc58d6277702c9538007
Lapololucy <lapololucy@gmail.com>
2023-04-11 09:01:35 -0700

    Task 10

--- 0x12-javascript-warm_up/10-factorial.js
+++ 0x12-javascript-warm_up/10-factorial.js
@@ -1,16 +1,16 @@
 #!/usr/bin/node
+
 function factorial(n) {
   if (isNaN(n)) {
     return 1;
-  } else if (n === 0) {
-    return 1;
-  } else {
-    return n * factorial(n - 1);
   }
+  if (n === 0) {
+    return 1;
+  }
+  return n * factorial(n - 1);
 }
 
-const arg = process.argv[2];
-const num = parseInt(arg);
+const arg = parseInt(process.argv[2]);
 
-console.log(factorial(Number(process.argv[2])));
+console.log(factorial(arg));
 
