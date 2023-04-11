d3039c32be6ed08f2be6f26bad3b5b5badce7af2
Lapololucy <lapololucy@gmail.com>
2023-04-11 01:10:44 -0700

    Task 17, updated script

---
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -0,0 +1,17 @@
+#!/usr/bin/node
+const myObject = {
+  type: 'object',
+  value: 12,
+  incr: function() {
+    this.value++;
+  }
+};
+
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+myObject.incr();
+console.log(myObject);
+
