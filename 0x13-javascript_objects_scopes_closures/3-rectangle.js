574c031d4d44a563fb6f35f5d4955856a704fc10
Lapololucy <lapololucy@gmail.com>
2023-04-12 00:23:34 -0700

    Task 3, a class of Rectange that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,18 +1,18 @@
 #!/usr/bin/node
 class Rectangle {
-	constructor(w, h) {
-		if (w <= 0 || h <= 0) {
-			return {}
-		}
-		this.width = w;
-		this.height = h;
-	}
+  constructor (w, h) {
+    if (w <= 0 || h <= 0) {
+	    return {};
+    }
+      this.width = w;
+      this.height = h;
+    }
 
-	print() {
-		fot (let i = 0; i < this.height; i++) {
-			console.log('X'.repeat(this.width));
-		}
-	}
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
 }
 
 module.exports = Rectangle;
