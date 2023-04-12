38453af05b03f9756742d7b84237ee4b2c647185
Lapololucy <lapololucy@gmail.com>
2023-04-12 00:15:44 -0700

    test

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,20 +1,18 @@
 #!/usr/bin/node
-
 class Rectangle {
-  constructor(w, h) {
-    if (w <= 0 || h <= 0) {
-      return {};
-    }
-    this.width = w;
-    this.height = h;
-  }
+	constructor(w, h) {
+		if (w <= 0 || h <= 0) {
+			return {}
+		}
+		this.width = w;
+		this.height = h;
+	}
 
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
-  }
+	print() {
+		fot (let i = 0; i < this.height; i++) {
+			console.log('X'.repeat(this.width));
+		}
+	}
 }
 
 module.exports = Rectangle;
-
