a6f4ce172eb8065cf9c485053638b412e4bdc8fa
Lapololucy <lapololucy@gmail.com>
2023-04-12 01:47:10 -0700

    Task 8, A function that returns the reversed version of a list

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,9 +1,4 @@
 #!/usr/bin/node
 exports.esrever = function (list) {
-  let reversedList = [];
-  for (let i = list.length - 1; i >= 0; i--) {
-    reversedList.push(list[i]);
-  }
-  return reversedList;
-}
-
+  return list.map((_, idx, arr) => arr[arr.length - 1 - idx]);
+};
