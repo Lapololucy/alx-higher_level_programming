8d9f11b5aa72b815b4c4e5b109c7dcbecf20c636
Lapololucy <lapololucy@gmail.com>
2023-04-12 00:25:55 -0700

    A class Rectangle that defines a rectangle

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,16 +1,17 @@
 #!/usr/bin/node
 class Rectangle {
   constructor (w, h) {
-    if (w <= 0 || h <= 0) {
-	    return {};
-    }
+    if (w > 0 && h > 0) {
       this.width = w;
       this.height = h;
     }
+  }
 
   print () {
     for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
+      const temp = [];
+      for (let j = 0; j < this.width; j++) temp.push('X');
+      console.log(`${temp.join('')}`);
     }
   }
 }
