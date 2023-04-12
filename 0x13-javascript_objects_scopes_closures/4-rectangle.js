318c3203eb8e2b7b537060a91afb49d504770941
Lapololucy <lapololucy@gmail.com>
2023-04-12 00:42:23 -0700

    Task 4

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,24 @@
+#!/usr/bin/node
+class Rectangle {
+  constructor (w, h) {
+    if (w > 0 && h > 0) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+	print () {
+    for (let i = 0; i < this.height; i++) {
+      const temp = [];
+      for (let j = 0; j < this.width; j++) temp.push('X');
+      console.log(`${temp.join('')}`);
+    }
+  }
+	rotate () {
+    [this.width, this.height] = [this.height, this.width];
+	}
+	double () {
+    this.width *= 2;
+    this.height *= 2;
+	}
+}
+module.exports = Rectangle;
