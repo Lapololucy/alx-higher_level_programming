a4188b7a86f5088ad920ba826674d0896c5afb32
Lapololucy <lapololucy@gmail.com>
2023-04-12 02:13:49 -0700

    Task 10 fixed

--- 0x13-javascript_objects_scopes_closures/10-converter.js
+++ 0x13-javascript_objects_scopes_closures/10-converter.js
@@ -1,7 +1,6 @@
 #!/usr/bin/node
 exports.converter = function (base) {
- return function (number) {
-  return Number(number).toString(base);
- };
+  return function (number) {
+    return Number(number).toString(base);
+  };
 };
-
