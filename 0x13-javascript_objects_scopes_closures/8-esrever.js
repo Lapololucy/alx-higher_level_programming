9688401ac9049e4d058540918640d3028381c884
Lapololucy <lapololucy@gmail.com>
2023-04-12 01:44:09 -0700

    Task 8

---
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -0,0 +1,9 @@
+#!/usr/bin/node
+exports.esrever = function (list) {
+  let reversedList = [];
+  for (let i = list.length - 1; i >= 0; i--) {
+    reversedList.push(list[i]);
+  }
+  return reversedList;
+}
+
