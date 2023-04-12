e930427cfade403c133bd0719f374fb68ad12a36
Lapololucy <lapololucy@gmail.com>
2023-04-12 02:26:14 -0700

    Task 11 done

--- 0x13-javascript_objects_scopes_closures/100-map.js
+++ 0x13-javascript_objects_scopes_closures/100-map.js
@@ -2,5 +2,5 @@
 const list = require('./100-data').list;
 
 const newList = list.map((value, index) => value * index);
-console.log('Initial list:', list);
-console.log('New list:', newList);
+console.log(list);
+console.log(newList);
