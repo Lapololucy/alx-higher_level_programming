7727a1f65c0c4ced76ee37a90dbdcce8e8d078c4
Lapololucy <lapololucy@gmail.com>
2023-04-15 23:44:22 -0700

    Task 9

--- 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
+++ 0x0F-python-object_relational_mapping/9-model_state_filter_a.py
@@ -1,18 +1,23 @@
 #!/usr/bin/python3
 """Lists all states via SQLAlchemy"""
 
+from sys import argv
+from model_state import Base, State
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker
-import sys
-from model_state import Base, State
-if __name__ == '__main__':
-    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-        sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
-    Base.metadata.create_all(engine)
-    Session = sessionmaker()
-    Session.configure(bind=engine)
-    session = Session()
-    for state in session.query(State).filter(State.name.like('%a%')):
-        print("{}: {}".format(state.id, state.name))
-    session.close()
 
+if __name__ == "__main__":
+    """
+    Access to the database and get a state
+    from the database.
+    """
+
+    db_uri = 'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
+        argv[1], argv[2], argv[3])
+    engine = create_engine(db_uri)
+    Session = sessionmaker(bind=engine)
+
+    session = Session()
+
+    for instance in session.query(State).filter(State.name.contains('a')):
+        print('{0}: {1}'.format(instance.id, instance.name))
