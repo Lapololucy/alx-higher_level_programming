83f618038f00f418f97ca24f1697377298baa99a
Lapololucy <lapololucy@gmail.com>
2023-04-15 23:29:52 -0700

    python object rational mapping

--- 0x0F-python-object_relational_mapping/relationship_state.py
+++ 0x0F-python-object_relational_mapping/relationship_state.py
@@ -5,6 +5,7 @@ a Base class to work with MySQLAlchemy ORM.
 """
 
 from sqlalchemy import Column, Integer, String
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -16,8 +17,10 @@ class State(Base):
         __tablename__ (str): The table name of the class
         id (int): The State id of the class
         name (str): The State name of the class
+        cities (:obj:`City`): The Cities belongs to State
     """
     __tablename__ = 'states'
 
     id = Column(Integer, primary_key=True)
     name = Column(String(128), nullable=False)
+    cities = relationship("City", backref="state", cascade="all, delete")
