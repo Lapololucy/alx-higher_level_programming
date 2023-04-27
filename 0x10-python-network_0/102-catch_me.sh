cd6a36dc783b3a74ee90dd2fc3f5f12d72bdb5a1
Lapololucy <lapololucy@gmail.com>
2023-04-27 02:30:58 -0700

    testing purpose

--- 0x10-python-network_0/102-catch_me.sh
+++ 0x10-python-network_0/102-catch_me.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 # script that makes a request to 0.0.0.0:5000/catch_me that causes the server to respond with a message containing You got me!, in the body of the response.
-curl -sL -X PUT -H "Origin: HolbertonSchool" -d "user_id=98" 0.0.0.0:5000/catch_me
+
+
+curl -s -X PUT -d "user_id=98" 0.0.0.0:5000/catch_me --output /dev/null -w "You got me!%{http_code}\n" | grep -o "You got me!"
+
