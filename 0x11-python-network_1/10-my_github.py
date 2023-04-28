c76c6d2e16e74aa6b80393cc358f8868866c515c
Lapololucy <lapololucy@gmail.com>
2023-04-28 02:42:43 -0700

    python networking 1

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,26 +1,16 @@
 #!/usr/bin/python3
+"""This script takes your GitHub credentials (username and password) and uses
+   the GitHub API to display your id
 """
-Module to access to the GitHub API and uses the information
-"""
+
 import requests
-from requests.auth import HTTPBasicAuth
-from sys import argv
+import sys
 
-
-def main(argv):
-    """
-    Script that takes your GitHub credentials (username and password) and
-    uses the GitHub API to display your id.
-    """
-    user = argv[1]
-    password = argv[2]
-    response = requests.get('https://api.github.com/user',
-                            auth=HTTPBasicAuth(user, password))
-    try:
-        profile_info = response.json()
-        print(profile_info['id'])
-    except:
-        print('None')
-
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    url = "https://api.github.com/users/{}".format(sys.argv[1])
+    headers = {
+                   "Accept": "application/vnd.github+json",
+                   "Authorization": "Bearer {}".format(sys.argv[2])
+              }
+    with requests.get(url, headers=headers) as response:
+        print(response.json().get('id'))
--- 0x11-python-network_1/5-hbtn_header.py
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -1,22 +1,11 @@
 #!/usr/bin/python3
+"""This script takes in a URL, sends a request to the URL and displays the
+   value of the X-Request-Id variable found in the header of the response.
 """
-Displays the value of the X-Request-Id variable
-found in the header of the response
-"""
+
 import requests
-from sys import argv
+import sys
 
-
-def main(argv):
-    """
-    Method that takes in a URL, sends a request to the URL
-    and displays the value of the X-Request-Id variable
-    found in the header of the response
-    """
-    url = argv[1]
-    r = requests.get(url)
-    headers = r.headers.get('X-Request-Id')
-    print(headers)
-
-if __name__ == "__main__":
-    main(argv)
+if __name__ == '__main__':
+    with requests.get(sys.argv[1]) as response:
+        print(response.headers.get("X-Request-Id"))
