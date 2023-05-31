e4e20f829e9c195e540b9b68c6325b3b119112fb
Lapololucy <lapololucy@gmail.com>
2023-05-31 07:24:31 +0000

    JavaScript script that updates the text of the <header> element to New Headergit add .!

---
+++ 0x15-javascript-web_jquery/6-script.js
@@ -0,0 +1,10 @@
+$(function () {
+  $("DIV#update_header").click(function () {
+    $("header").text("New Header!!!");
+  });
+});
+
+//OR
+//$('div#update_header').click(function () {
+//    $('header').text('New Header!!!');
+//});
